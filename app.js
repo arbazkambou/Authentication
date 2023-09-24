fa26bb5a7bdc0db3a7f7a149161694622dd54f55
Arbaz Shoukat <arbazkamboh342@gmail.com>
2023-09-24 21:38:27 +0500

    Level 6 OAuth authentication  using google

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose=require("mongoose");
 const session=require("express-session");
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate=require("mongoose-findorcreate");
 
 const app=express();
 
@@ -27,15 +29,42 @@ mongoose.connect("mongodb://127.0.0.1:27017/secretDB");
 
 const usersSchema=new mongoose.Schema({
     username:String,
-    password:String
+    password:String,
+    googleId:String
 });
 
 usersSchema.plugin(passportLocalMongoose);
+usersSchema.plugin(findOrCreate);
 const User=new mongoose.model("User",usersSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/",(req,res)=>{
@@ -47,6 +76,15 @@ app.get("/login",(req,res)=>{
 app.get("/register",(req,res)=>{
     res.render("register");
 });
+app.get("/auth/google",(req,res)=>{
+    passport.authenticate('google',{ scope: ["profile"] })(req,res);
+});
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 app.get("/secrets",(req,res)=>{
     if(req.isAuthenticated()){
         res.render("secrets");
